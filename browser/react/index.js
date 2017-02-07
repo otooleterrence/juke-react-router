f1f3d92d99bb75dfc70459dcbc54e24231e08682
Benjamin <benjamin.gu100@gmail.com>
2017-02-07 18:33:36 -0500

    Up to React CloneDeepElement

--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import {Route, Router, hashHistory} from 'react-router';
 
 ReactDOM.render(
-  <AppContainer />,
+	<div>
+  <Router history={hashHistory}>
+  <Route path = '/' component = {AppContainer}>
+  </Route>
+  </Router>
+  </div>,
   document.getElementById('app')
 );
